09757711ca288ba7d3694a56f6d84e5b1d7a2c39
Sergey Romanenko <sergey.romanenko@flextype.org>
2022-01-21 22:50:06 +0300

    chore(core): typo updates

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 export default function browser() {
     const browsers = [
-        "Chrome",
-        "Edge",
-        "Firefox",
-        "Internet Explorer",
-        "Safari",
-        "Opera",
-        "Yandex",
-        "Chromium",
-        "Vivaldi",
-        "Baidu",
-        "Brave"
+        'Chrome',
+        'Edge',
+        'Firefox',
+        'Internet Explorer',
+        'Safari',
+        'Opera',
+        'Yandex',
+        'Chromium',
+        'Vivaldi',
+        'Baidu',
+        'Brave',
     ];
     return browsers[Math.floor(Math.random() * browsers.length)];
 }
