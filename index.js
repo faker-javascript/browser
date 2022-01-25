3232c3f2328684a4edd50dd65c61fa8a7482ec67
Sergey Romanenko <sergey.romanenko@flextype.org>
2022-01-25 17:40:39 +0300

    refactor(core): improve and simplify logic

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
+import path from 'node:path';
+import fs from 'node:fs';
+import {loadJsonFileSync} from 'load-json-file';
+
 export default function browser() {
-    const browsers = [
-        'Chrome',
-        'Edge',
-        'Firefox',
-        'Internet Explorer',
-        'Safari',
-        'Opera',
-        'Yandex',
-        'Chromium',
-        'Vivaldi',
-        'Baidu',
-        'Brave',
-    ];
+    const filePath = `./browsers.json`;
+    let browsers = [];
+    browsers = fs.existsSync(path.resolve(filePath)) ? loadJsonFileSync(filePath) : loadJsonFileSync(path.resolve('node_modules/@fakerjs/browser/', filePath));
     return browsers[Math.floor(Math.random() * browsers.length)];
 }
